ea4dcd37aef372c859ccf5d03e8ee41eae06e181
Gabriel Loza Lucero <sergio.loza.l@ucb.edu.bo>
2020-12-03 15:25:40 -0400

    service and model city added

---
+++ src/app/city/model/city.ts
@@ -0,0 +1,13 @@
+export class City{
+    cityId: number;
+    name: String;
+    status: number;
+
+    constructor(cityId: number, name: String, status: number){
+        this.cityId = cityId;
+        this.name = name;
+        this.status = status;
+    }
+
+
+}
---
+++ src/app/city/serviceCity/service.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs";
+import { Injectable } from '@angular/core';
+import { City } from '../model/city';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+  constructor(private http:HttpClient) { }
+
+  Url:string = 'http://localhost:8080/v1/city/';
+  getCity(){
+  return this.http.get<City[]>(this.Url);
+  }
+
+
+}
