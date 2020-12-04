e053995c3fb7d986189ee7d8a4205224f4f9bdf4
rodriguez-luis <pb.rndm@gmail.com>
2020-12-04 17:31:01 -0400

    Add cart button

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { ProductService } from './product.service';
 import { MatDialog } from '@angular/material/dialog';
+import { CartService } from '../cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -9,7 +12,7 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class ProductComponent implements OnInit {
   product: any; 
-  constructor(private productService: ProductService, private dialog: MatDialog ) { }
+  constructor(private productService: ProductService, private dialog: MatDialog, public cartService: CartService, private snackBar: MatSnackBar, private router: Router ) { }
 
   ngOnInit(): void {
     this.product = this.productService.getLast();
@@ -19,5 +22,14 @@ export class ProductComponent implements OnInit {
   }
   close(){
     this.dialog.closeAll();
+  }
+  addToCart(product: Product){
+    this.cartService.addProduct(product);
+    let sb = this.snackBar.open("Producto añadido","Ver carrito", {
+      duration: 2000,
+    });
+    sb.onAction().subscribe(() => {
+       this.router.navigateByUrl('/cart')
+    });
   }
 }
