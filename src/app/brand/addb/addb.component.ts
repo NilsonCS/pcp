6de9f710a18cb41739ae9e4f6e9672865f4a9ea1
Nilson <mgtalsc@gmail.com>
2020-12-17 00:16:18 -0400

    listarcompononet

--- src/app/brand/addb/addb.component.ts
+++ src/app/brand/addb/addb.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {MatDialog} from '@angular/material/dialog';
+import {ServiceService} from '../../brand/service.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-addb',
@@ -6,10 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addb.component.css']
 })
 export class AddbComponent implements OnInit {
+  public formGroup: any;
+  titleAlert = 'Este campo es requerido.';
+  post: any = '';
+  total = 0;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private dialog: MatDialog, public service: ServiceService, private router: Router) { }
 
   ngOnInit(): void {
+    this.createForm();
+    this.setChangeValidate();
   }
+
+  createForm() {
+    this.formGroup = this.formBuilder.group({
+
+      name: [null, Validators.required],
+    });
+  }
+
+  setChangeValidate() {
+    this.formGroup.get('validate').valueChanges.subscribe((validate: any) => {
+      if (validate == '1') {
+        this.formGroup
+          .get('name')
+          .setValidators([Validators.required, Validators.minLength(5)]);
+        this.titleAlert = 'You need to specify at least 5 characters';
+      } else {
+        this.formGroup.get('name').setValidators(Validators.required);
+      }
+      this.formGroup.get('name').updateValueAndValidity();
+    });
+  }
+  get name() {
+    return this.formGroup.get('name') as FormControl;
+  }
+
+  onSubmit(post: any) {
+    this.service.createBrand({ name: post.name}).subscribe(data => { alert('La Empresa se guardo exitosamente') ; });
+
+  }
+
+
+  Listar(){
+    this.router.navigate(['listarb']);
+  }
+
+  Nuevo(){
+    this.router.navigate(['addb']);
+  }
+
+
 
 }
--- src/app/brand/editb/editb.component.ts
+++ src/app/brand/editb/editb.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {MatDialog} from '@angular/material/dialog';
+import {ServiceService} from '../../brand/service.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-editb',
@@ -7,9 +11,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditbComponent implements OnInit {
 
-  constructor() { }
+  public formGroup: any;
+  titleAlert = 'This field is required';
+  post: any = '';
+  total = 0;
+
+  constructor(private formBuilder: FormBuilder, private dialog: MatDialog, public service: ServiceService, private router: Router, private activerouter: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.createForm();
+    this.setChangeValidate();
+    this.Edit();
   }
+  createForm() {
+    this.formGroup = this.formBuilder.group({
+      name: [null, Validators.required]
+    });
+  }
+
+
+
+
+
+
+  setChangeValidate() {
+    this.formGroup.get('validate').valueChanges.subscribe((validate: any) => {
+      if (validate == '1') {
+        this.formGroup
+          .get('name')
+          .setValidators([Validators.required, Validators.minLength(5)]);
+        this.titleAlert = 'You need to specify at least 5 characters';
+      } else {
+        this.formGroup.get('name').setValidators(Validators.required);
+      }
+      this.formGroup.get('name').updateValueAndValidity();
+    });
+  }
+  get name() {
+    return this.formGroup.get('name') as FormControl;
+  }
+
+
+  datosBrand: any;
+
+  editarForm = new FormGroup({
+    brandId: new FormControl(''),
+    name: new FormControl(''),
+  });
+
+
+  Edit(){
+    let brandId = this.activerouter.snapshot.paramMap.get('id');
+    this.service.getBrandId(brandId).subscribe(data => {
+
+      this.datosBrand = data;
+      this.editarForm.setValue({
+        'brandId': brandId,
+        'name': this.datosBrand.name,
+      });
+      console.log(data);
+
+    });
+  }
+  brand:any;
+  actualizarBrand(brand: any){
+    this.service.updateBrand(brand)
+      .subscribe( data => {
+        this.brand = data;
+        alert('Compañia Actualizada Exitosamente');
+        this.router.navigate(['listarb']);
+        console.log(data);
+      });
+  }
+
+
+
+  Listar(){
+    this.router.navigate(['listarb']);
+  }
+
+
 
 }
